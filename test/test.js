11e6233daef9e328ad0ad9c3f19d1abcb3cc6c88
jimmyfargo <zblizzard@live.com>
2017-03-08 23:04:11 -0600

    adding case to check if two responses bring back the same json object

--- test/test.js
+++ test/test.js
@@ -9,35 +9,46 @@ describe('return luke', () => {
         request.get({ url: baseUrl + '/people/1/' },
             (err, resp, body) => {
                 var luke = JSON.parse(body);
-                expect(luke.name).to.equal('Luke Skywalker');
-                expect(luke.hair_color).to.equal('blond');
                 expect(resp.statusCode).to.equal(200);
-                // console.log(JSON.stringify(luke, null, 2));
+                checkIfObjIsTheSame(luke);
                 done();
             });
     });
 });
 
-// with chakram
-describe('return luke with chakram', () => {
-    it('should return luke skywalker', () => {
-        return chakram.get(baseUrl + '/people/1/').then((resp) => {
-            var luke = resp.body;
-            expect(luke.name).to.equal('Luke Skywalker');
-            expect(resp.response.statusCode).to.equal(200);
+function checkIfObjIsTheSame(old) {
+    describe('checking if obj is the same', () => {
+        it('should be the same', (done) => {
+            request.get({ url: baseUrl + '/people/1/' }, 
+                (err, response, body) => {
+                    expect(JSON.parse(body)).to.deep.equal(old);
+                    done();
+                });
         });
     });
-});
+}
 
-describe('return user', () => {
-    it('should return a user', (done) => {
-        request.get({ url: 'http://localhost:3090/user/eb1a1663-476d-4870-9a43-8e58acf15ccd' }, 
-            (err, resp, body) => {
-                var user = JSON.parse(body);
-                expect(resp.statusCode).to.equal(200);
-                expect(user.user_name).to.equal('jimmyfargo');
-                // console.log(JSON.stringify(user, null, 2));
-                done();
-            });
-    });
-});
+
+// with chakram
+// describe('return luke with chakram', () => {
+//     it('should return luke skywalker', () => {
+//         return chakram.get(baseUrl + '/people/1/').then((resp) => {
+//             var luke = resp.body;
+//             expect(luke.name).to.equal('Luke Skywalker');
+//             expect(resp.response.statusCode).to.equal(200);
+//         });
+//     });
+// });
+
+// describe('return user', () => {
+//     it('should return a user', (done) => {
+//         request.get({ url: 'http://localhost:3090/user/eb1a1663-476d-4870-9a43-8e58acf15ccd' }, 
+//             (err, resp, body) => {
+//                 var user = JSON.parse(body);
+//                 expect(resp.statusCode).to.equal(200);
+//                 expect(user.user_name).to.equal('jimmyfargo');
+//                 // console.log(JSON.stringify(user, null, 2));
+//                 done();
+//             });
+//     });
+// });
